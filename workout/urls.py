a2efe7046088cb43ee101ded21a59a6488cf48af
UkiHong <suhong9507@gmail.com>
2023-04-28 17:07:17 +0100

    Sentry SDK

--- workout/urls.py
+++ workout/urls.py
@@ -11,4 +11,5 @@ urlpatterns = [
     path("<int:pk>/bookings/check", views.WorkoutBookingCheck.as_view()),
     path("preparations", views.Preparations.as_view()),
     path("preparations/<int:pk>", views.PreparationDetail.as_view()),
+    path("make-error", views.make_error),
 ]
