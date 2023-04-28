37f8b3160cb75b51c9f180bb523b224faf2d77ff
UkiHong <suhong9507@gmail.com>
2023-04-28 16:05:24 +0100

    4

--- config/settings.py
+++ config/settings.py
@@ -189,9 +189,6 @@ CORS_ALLOWED_ORIGINS = ["http://127.0.0.1:3000"]
 CORS_ALLOW_CREDENTIALS = True
 CSRF_TRUSTED_ORIGINS = ["http://127.0.0.1:3000"]
 
-Access_Control_Allow_Origin = ["http://127.0.0.1:3000"]
-Access_Control_Allow_Credentials = True
-
 
 CF_ID = env("CF_ID")
 CF_TOKEN = env("CF_TOKEN")
