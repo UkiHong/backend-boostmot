a32dc344b4df33dda23f64e15803d853543374d5
UkiHong <suhong9507@gmail.com>
2023-04-28 20:29:11 +0100

    backend.boostmot.xyz add

--- config/settings.py
+++ config/settings.py
@@ -34,10 +34,7 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = "RENDER" not in os.environ
 
-ALLOWED_HOSTS = [
-    "localhost",
-    "127.0.0.1",
-]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "backend.boostmot.xyz"]
 
 RENDER_EXTERNAL_HOSTNAME = os.environ.get("RENDER_EXTERNAL_HOSTNAME")
 
