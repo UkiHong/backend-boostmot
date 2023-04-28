cb3583a5c0b96b519d46b33c91bbaf1663acb9f0
UkiHong <suhong9507@gmail.com>
2023-04-28 15:50:35 +0100

    4

--- build.sh
+++ build.sh
@@ -1,11 +1,9 @@
 #!/usr/bin/env bash
 # exit on error
 set -o errexit
+pip install --upgrade pip
 
 poetry install
-
+pip install --force-reinstall -U setuptools
 python manage.py collectstatic --no-input
 python manage.py migrate
-
-pip install --upgrade pip
-pip install --force-reinstall -U setuptools
