ec7b8f0111b11cc8b7ccf0880919982935f35b73
UkiHong <suhong9507@gmail.com>
2023-04-28 15:42:23 +0100

    1

--- build.sh
+++ build.sh
@@ -2,14 +2,7 @@
 # exit on error
 set -o errexit
 
-echo "Installing the latest version of poetry..."
+poetry install
 
-pip install --upgrade pip
-
-pip install poetry==1.2.0
-
-rm poetry.lock
-
-poetry lock
-
-python -m poetry install
+python manage.py collectstatic --no-input
+python manage.py migrate
