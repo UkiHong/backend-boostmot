e94883f9d3c2bdb2d958301e682afd7d782c90d3
UkiHong <suhong9507@gmail.com>
2023-04-28 22:29:22 +0100

    authentication

--- config/settings.py
+++ config/settings.py
@@ -194,9 +194,7 @@ else:
     CSRF_TRUSTED_ORIGINS = ["https://boostmot-frontend.onrender.com"]
 
 CORS_ORIGIN_ALLOW_ALL = True
-CORS_ORIGIN_WHITELIST = [
-    "https://boostmot.xyz",
-]
+
 CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOW_METHODS = [
     "DELETE",
